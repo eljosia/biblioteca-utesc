aa45005d492e7d5d7cb28d509d43765556dbd35a
Jose A. Rodriguez <angel@teknik.mx>
2024-07-19 00:44:41 -0700

    UPDATE METRONIC

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\ConfigController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GuestController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\PeopleController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +28,30 @@ Route::get('/', [GuestController::class, 'index'])->name('guest.index');
 Route::middleware(['auth', 'verified'])->group(function () {
     // DASHBOARD
     Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard.index');
+
+    // Libros
+    Route::get('/libros', [BooksController::class, 'index'])->name('book.index');
+    Route::get('/libro/nuevo', [BooksController::class, 'new'])->name('book.new');
+    Route::get('/libro/editar/{id?}', [BooksController::class, 'edit'])->name('book.edit');
+
+    // Prestamos
+    Route::get('/prestamos', [LoanController::class, 'index'])->name('loan.index');
+    Route::get('/prestamos/nuevo', [LoanController::class, 'new'])->name('loan.new');
+    Route::get('/prestamos/editar/{code?}', [LoanController::class, 'edit'])->name('loan.edit');
+    Route::get('/prestamo/{code?}', [LoanController::class, 'show'])->name('loan.show');
+    Route::get('/prestamo/print/{code?}', [LoanController::class, 'print'])->name('loan.print');
+    Route::get('/prestamo/print/voucher/{code?}', [LoanController::class, 'delivery_voucher'])->name('loan.print_delivery_voucher');
+
+    // Reportes
+    Route::get('/reportes', [ReportController::class, 'index'])->name('report.index');
+
+    // Personas
+    Route::get('/personas', [PeopleController::class, 'index'])->name('people.index');
+    Route::get('/personas/nuevo', [PeopleController::class, 'new'])->name('people.new');
+    Route::get('/persona/editar/{identifier?}', [PeopleController::class, 'edit'])->name('people.edit');
+
+    // Configuracion
+    Route::get('/configuracion', [ConfigController::class, 'index'])->name('config.index');
 });
 
 Route::get('/error', function () {
