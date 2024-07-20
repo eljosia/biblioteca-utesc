d3c3ff2736ebbb53bfde227fce5bcf2ff2ddd68c
Jose A. Rodriguez <angel@teknik.mx>
2024-07-19 23:20:54 -0700

    Login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = (object)[];
+        $data->page_title = "Dashboard";
+        $data->breadcrumb = [
+            ['link' => route('dashboard.index'), 'text' => 'Dashboard'],
+            ['text' => 'Inicio']
+        ];
+        $data->buttons = [
+            ['modal' => 'kt_modal_create_app', 'class' => 'btn btn-sm fw-bold bg-body btn-color-gray-700 btn-active-color-primary', 'text' => 'Rollover'],
+            ['link' => '#link', 'class' => 'btn btn-sm fw-bold btn-primary', 'text' => 'Boton', 'icon' => 'la-pen']
+        ];
+        return view('pages.dashboards.index', compact('data'));
     }
 }
