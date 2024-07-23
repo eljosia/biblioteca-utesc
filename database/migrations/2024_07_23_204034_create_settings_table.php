722dea2ae419fd774c2d081ecbd2a8a06e8cb23d
Jose A. Rodriguez <angel@teknik.mx>
2024-07-23 15:28:01 -0700

    CRUD: settings sin terminar

---
+++ database/migrations/2024_07_23_204034_create_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('header_membrete');
+            $table->string('footer_membrete');
+            $table->unsignedBigInteger('created_by')->nullable()->default(1);
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
